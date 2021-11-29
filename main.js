cb8d0884c01f03a1511bf86529dfee350eaca543
SalimKobi <salimkobi9@gmail.com>
2021-11-29 01:27:29 +0100

    completed html and css of the first section of the image slider

---
+++ main.js
@@ -0,0 +1,5 @@
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
+const images = document.querySelector(".images").children;
+const totalImages = images.length;
+let index = 0; 
