6bcd47db5e818583e4c471c30b6a793a00ccb86d
SalimKobi <salimkobi9@gmail.com>
2021-12-08 21:55:43 +0100

    project completed, refactoring remaining

--- main.js
+++ main.js
@@ -2,8 +2,39 @@
 document.querySelector(".submit2")
 document.querySelector(".information").textContent = "Let's go!"
 document.querySelector(".next").classList.add("display");
+let clickNo = 0;
 
 
+
+function youWon() {
+    document.querySelector(".container").style.backgroundColor = "#60b347";
+    document.querySelector(".playerStat").textContent = "Congratulations you won 🎉"
+    document.querySelector(".playerStat").style.color = "white"
+    document.querySelector(".playerStat").style.fontSize = "5rem"
+    document.querySelector(".image").classList.add("display");
+    document.querySelector(".image2").classList.add("display");
+    document.querySelector(".submit2").classList.add("display");
+    document.querySelector(".submit").classList.add("display");
+    document.querySelector(".name").classList.add("display");
+    document.querySelector(".name2").classList.add("display");
+    document.querySelector(".points").classList.add("display");
+    document.querySelector(".next").classList.add("display");
+}
+
+function youLost() {
+    document.querySelector(".container").style.backgroundColor = "red";
+    document.querySelector(".playerStat").textContent = "Sorry you lost 😢"
+    document.querySelector(".playerStat").style.color = "white"
+    document.querySelector(".playerStat").style.fontSize = "5rem"
+    document.querySelector(".image").classList.add("display");
+    document.querySelector(".image2").classList.add("display");
+    document.querySelector(".submit2").classList.add("display");
+    document.querySelector(".submit").classList.add("display");
+    document.querySelector(".name").classList.add("display");
+    document.querySelector(".name2").classList.add("display");
+    document.querySelector(".points").classList.add("display");
+    document.querySelector(".next").classList.add("display");
+}
 
 function displayNext() {
     const nextButton = document.querySelector(".next");
@@ -81,6 +112,18 @@ let arr = [{
     "name": "Wild Dog",
     "image": "img/wild_dog.jpg",
     "speed": 71
+}, {
+    "name": "Ostritch",
+    "image": "img/ostritch.jpg",
+    "speed": 70
+}, {
+    "name": "Jaguar",
+    "image": "img/jaguar.jpg",
+    "speed": 80
+}, {
+    "name": "Tiger",
+    "image": "img/Tiger.jpg",
+    "speed": 65
 }];
 
 
@@ -91,16 +134,6 @@ function getRand() {
     let rand = Math.floor(Math.random() * (max - min) + min);
     return rand;
 }
-
-// Generate randon numbers for the left and right images 
-// numberLeft = getRand();
-// numberRight = getRand();
-
-// function generating random numbers 
-// function generateNum(){
-//     numberLeft = getRand();
-//     numberRight = getRand();
-// }
 
 
 function genrateNumImg() {
@@ -146,185 +179,80 @@ function Next() {
         document.querySelector(".name2").textContent = arr[numberRight].name
         // Adding actions if the user clicks on the first button
         document.querySelector(".submit").addEventListener('click', function () {
+            clickNo++;
+            console.log(clickNo)
             disableAllbtn();
             displayNext()
             if (arr[numberLeft].speed > arr[numberRight].speed) {
-                document.querySelector(".container").style.backgroundColor = "#60b347";
                 score++;
-                document.querySelector(".score").textContent = score;
-                document.querySelector(".information").textContent = `The ${arr[numberLeft].name} moves at ${arr[numberLeft].speed}km/hr while the ${arr[numberRight].name} moves at ${arr[numberRight].speed}km/hr`
+                if (clickNo === 5) {
+                    console.log("click is up to 5")
+                    if (score > 4) {
+                        youWon()
+                    } else {
+                        youLost()
+                        console.log("you lost")
+                    }
+                } else {
+                    document.querySelector(".container").style.backgroundColor = "#60b347";
+                    document.querySelector(".score").textContent = score;
+                    document.querySelector(".information").textContent = `The ${arr[numberLeft].name} moves at ${arr[numberLeft].speed}km/hr while the ${arr[numberLeft].name} moves at ${arr[numberLeft].speed}km/hr`
+                }
             } else {
-                document.querySelector(".container").style.backgroundColor = "red";
-                document.querySelector(".information").textContent = `The ${arr[numberLeft].name} moves at ${arr[numberLeft].speed}km/hr while the ${arr[numberRight].name} moves at ${arr[numberRight].speed}km/hr`
-            }
-        })
-
-        //Adding actions if the user clicks on the first button
-        document.querySelector(".submit2").addEventListener('click', function () {
-            disableAllbtn();
-            displayNext()
-            if (arr[numberRight].speed > arr[numberLeft].speed) {
-                document.querySelector(".container").style.backgroundColor = "#60b347";
-                score++;
-                document.querySelector(".score").textContent = score;
-                document.querySelector(".information").textContent = `The ${arr[numberRight].name} moves at ${arr[numberRight].speed}km/hr while the ${arr[numberLeft].name} moves at ${arr[numberLeft].speed}km/hr`
-
-            } else {
-                document.querySelector(".container").style.backgroundColor = "red";
-                document.querySelector(".information").textContent = `The ${arr[numberRight].name} moves at ${arr[numberRight].speed}km/hr while the ${arr[numberLeft].name} moves at ${arr[numberLeft].speed}km/hr`
+                if (clickNo === 5) {
+                    console.log("click is up to 5")
+                    youLost()
+                } else {
+                    document.querySelector(".container").style.backgroundColor = "red";
+                    document.querySelector(".information").textContent = `The ${arr[numberLeft].name} moves at ${arr[numberLeft].speed}km/hr while the ${arr[numberRight].name} moves at ${arr[numberRight].speed}km/hr`
+                }
 
             }
             // console.log(score);
         })
-        // console.log(score);
+
+        //Adding actions if the user clicks on the first button
+        document.querySelector(".submit2").addEventListener('click', function () {
+            clickNo++;
+            console.log(clickNo)
+            disableAllbtn();
+            displayNext()
+            if (arr[numberRight].speed > arr[numberLeft].speed) {
+                score++;
+                if (clickNo === 5) {
+                    console.log("click is up to 5")
+                    if (score > 4) {
+                        youWon()
+                    } else {
+                        youLost()
+                        console.log("you lost")
+                    }
+                } else {
+                    document.querySelector(".container").style.backgroundColor = "#60b347";
+                    document.querySelector(".score").textContent = score;
+                    document.querySelector(".information").textContent = `The ${arr[numberRight].name} moves at ${arr[numberRight].speed}km/hr while the ${arr[numberLeft].name} moves at ${arr[numberLeft].speed}km/hr`
+                }
+            } else {
+                if (clickNo === 5) {
+                    console.log("click is up to 5")
+                    youLost()
+                } else {
+                    document.querySelector(".container").style.backgroundColor = "red";
+                    document.querySelector(".information").textContent = `The ${arr[numberRight].name} moves at ${arr[numberRight].speed}km/hr while the ${arr[numberLeft].name} moves at ${arr[numberLeft].speed}km/hr`
+                }
+
+            }
+        })
+
 
     }
     // document.querySelector(".score").textContent = score;
     document.querySelector(".next").classList.add("display");
     enableAllbtns()
 
+
 }
-//TRIAL TRIAL TRIAL TRIAL TRIAL TRIAL TRIAL TRIAL
+
 
 Next();
-
-// function getR() {
-//     let min = 0;
-//     let max = arr.length;
-//     let rand = Math.floor(Math.random() * (max - min) + min);
-//     return rand;
-// }
-// numberA = getR();
-// numberB = getR();
-
-// while (numberA === numberB) {
-//     numberA = getR();
-//     numberB = getR();
-
-// }
-// console.log(numberA)
-// console.log(numberB)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        // document.querySelector(".name").textContent = arr[numberLeft].name
-
-        // console.log(numberLeft)
-
-
-
-
-
-
-
-//CONSULT THIS TO BUILD LOGIC CONSULT THIS TO BUILD LOGIC CONSULT THIS TO BUILD LOGIC CONSULT THIS TO BUILD LOGIC CONSULT THIS TO BUILD LOGIC CONSULT THIS TO BUILD LOGIC
-// if (question = 1) {
-//     let question = 1;
-//     document.querySelector(".submit").addEventListener('click', function () {
-//         console.log("this is the right answer");
-//         score++
-//         document.querySelector(".container").style.backgroundColor = "#60b347"
-//         document.querySelector(".score").textContent = score;
-//         document.querySelector(".information").textContent = "The golden eagle is faster"
-//         question++;
-//     })
-// }
-
-
-// document.querySelector(".submit2").addEventListener('click', function () {
-//     console.log("this is the wrong answer");
-//     document.querySelector(".container").style.backgroundColor = "red"
-//     document.querySelector(".score").textContent = score;
-//     document.querySelector(".information").textContent = "The is slower than the golden eagle"
-// })
-// CONSULT THIS TO BUILD LOGIC CONSULT THIS TO BUILD LOGIC CONSULT THIS TO BUILD LOGIC CONSULT THIS TO BUILD LOGIC CONSULT THIS TO BUILD LOGIC 
-
-
-
-// // //
-
-// // // Logic for the left side section of the app
-// // prev.addEventListener("click", () => {
-// //     nextImage('next');
-// // })
-
-// // next.addEventListener("click", () => {
-// //     nextImage("prev")
-// // })
-
-// // function nextImage(direction) {
-// //     if (direction == "next") {
-// //         index++;
-// //         console.log(index)
-// //         if (index == totalImages) {
-// //             index = 0;
-// //         }
-// //     } else {
-// //         if (index == 0) {
-// //             index = totalImages - 1;
-// //         } else {
-// //             index--;
-// //         }
-// //     }
-
-// //     for (let i = 0; i < images.length; i++) {
-// //         images[i].classList.remove('show');
-// //     }
-// //     images[index].classList.add('show');
-// // }
-
-
-// // // parameters for right section of the app
-// // const prev2 = document.querySelector(".prev2");
-// // const next2 = document.querySelector(".next2");
-// // const images2 = document.querySelector(".images2").children;
-// // const submit2 = document.querySelector(".submit2")
-// // let result2 = document.querySelectorAll(".name2");
-
-// // let index2 = 0;
-
-// // // Logic for the right side section of the app
-// // prev.addEventListener("click", () => {
-// //     nextImage('next2');
-// // })
-
-// // next.addEventListener("click", () => {
-// //     nextImage("prev2")
-// // })
-
-// // function nextImage(direction) {
-// //     if (direction == "next") {
-// //         index2++;
-// //         console.log(index)
-// //         if (index2 == totalImages) {
-// //             index2 = 0;
-// //         }
-// //     } else {
-// //         if (index2 == 0) {
-// //             index2 = totalImages - 1;
-// //         } else {
-// //             index2--;
-// //         }
-// //     }
-
-// //     for (let i = 0; i < images.length; i++) {
-// //         images[i].classList.remove('show');
-// //     }
-// //     images[index2].classList.add('show');
-// // }
 
